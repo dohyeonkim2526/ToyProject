68ae02b08524fcd4c1507a13ff731a1a87f2324c
dohyeonkim2526 <veronica.kdh@gmail.com>
2023-11-14 20:30:59 +0900

    find imdb site's score with netflix films

---
+++ main.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import numpy as np
+import pymysql
+import mysql_connect # mysql 접속정보
+from sqlalchemy import create_engine
+
+login = mysql_connect.info
+
+# MySQL 서버 연결
+conn = pymysql.connect(host = login['host'],
+                       port = login['port'],
+                       user = login['username'],
+                       password = login['password'],
+                       db = 'ImdbDB',
+                       charset = login['charset'])
+
+# MySQL 서버와 연결할 수 있는 cursor 생성
+cursor = conn.cursor()
+
+# read csv
+result_df = pd.read_csv('/Users/gimdohyeon/Documents/스터디/dataset/result_df.csv')
+result_df = result_df[['Imdb_code', 'Title', 'Imdb_Premiere', 'Imdb_Age', 'Imdb_Time', 'IMDB Score', 'Language', 'Nflx_Genre1', 'Nflx_Genre2', 'Nflx_Genre3', 'Nflx_Genre4']] # 컬럼 순서 변경
+result_df = result_df.replace({np.nan: None})
+
+sql = """INSERT INTO ImdbDB.films (imdb_code, title, imdb_premiere, imdb_age, imdb_time, imdb_score, imdb_language, nflx_genre1, nflx_genre2, nflx_genre3, nflx_genre4) 
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"""
+
+for index, row in result_df.iterrows():
+    cursor.execute(sql, tuple(row))
+
+conn.commit()
+
+# cursor 닫기
+cursor.close()
+# db connection 닫기
+conn.close()
+
+
+
---
+++ mysql_connect.py
@@ -0,0 +1,7 @@
+info = {"host" : "localhost",
+        "port" : 3306,
+        "username" : "admin",
+        "password" : "admin1234",
+        "charset" : "utf8"
+}
+
