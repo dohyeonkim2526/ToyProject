f2fe626aaf67597b2b73541dd753beab834dd4d1
dohyeonkim2526 <veronica.kdh@gmail.com>
2023-11-20 11:37:34 +0900

    complete make film web page and show the detail of film

--- ImdbScores/main.py
+++ ImdbScores/main.py
@@ -24,6 +24,7 @@ conn = pymysql.connect(host = login['host'],
 # MySQL 서버와 연결할 수 있는 cursor 생성
 cursor = conn.cursor()
 
+
 # read csv
 result_df = pd.read_csv('/Users/gimdohyeon/Documents/스터디/dataset/result_df.csv')
 result_df = result_df[['Imdb_code', 'Title', 'Imdb_Premiere', 'Imdb_Age', 'Imdb_Time', 'IMDB Score', 'Language', 'Nflx_Genre1', 'Nflx_Genre2', 'Nflx_Genre3', 'Nflx_Genre4']] # 컬럼 순서 변경
@@ -44,8 +45,8 @@ conn.close() # db connection 닫기
 
 
 
-# Imdb 사이트 크롤링 : imdb_code를 이용해서 나라별 평가 점수와 포스터 이미지 수집
-
+# Imdb 사이트 크롤링
+# imdb_code를 이용해서 나라별 평가 점수와 포스터 이미지 수집
 country_dict = {}  # (key: imdb_code, value: imdb site's json)
 poster_dict = {} # (key: imdb_code, value: poster img)
 error = list()
@@ -85,7 +86,7 @@ for index, row in tqdm(result_df.iterrows(), total = result_df.shape[0]):
         print('error')
 
 
-# db upload
+# Imdb 사이트에서 수집한 정보 db upload
 cursor = conn.cursor()
 cursor.execute("""TRUNCATE TABLE ImdbDB.imdb_score""")
 insert_sql = """INSERT INTO ImdbDB.imdb_score(imdb_code, json_data) VALUES (%s, %s)"""
@@ -94,8 +95,7 @@ update_sql = """UPDATE ImdbDB.imdb_score
                     WHERE imdb_code = %s"""
 
 
-# insert mysql with ratings info(json) and poster image
-for key, value in country_dict.items():
+for key, value in country_dict.items(): # insert mysql with ratings info(json) and poster image
     code = key
     json_data = json.dumps(value)
 
@@ -109,8 +109,8 @@ for key, value in country_dict.items():
 conn.commit()
 
 
-
-# table join : 영화에 대한 상세정보, json data, 포스터를 모두 확인할 수 있는 테이블 생성
+# 영화에 대해 수집한 모든 정보를 가지는 Main 테이블 생성 > Flask를 연동해서 웹페이지 보여줄 때 사용할 테이블
+# JOIN - 영화정보, Json data, 포스터이미지
 cursor = conn.cursor()
 cursor.execute("""CREATE TABLE ImdbDB.films_info AS
                     SELECT A.imdb_code
@@ -128,145 +128,89 @@ conn.commit()
 
 cursor.close()
 conn.close()
-
-
-# graph 연습
-import matplotlib.pyplot as plt
-data
-test = data
-json = json.loads(data[0][-1])
-
-countries = [j['displayText'] for j in json]
-rates = [j['aggregateRating'] for j in json]
-counts = [j['totalVoteCount'] for j in json]
-
-plt.bar(ocuntries, rates)import pandas as pd
-import numpy as np
-import pymysql
-import requests
-import json
-from lxml import etree
-from bs4 import BeautifulSoup
-from tqdm import tqdm
-import sys
-import time
-sys.path.insert(0, './ImdbScores') # mysql 접속정보 저장한 폴더경로 연결
-import mysql_connect # mysql 접속정보
-
-login = mysql_connect.info
-
-# MySQL 서버 연결
-conn = pymysql.connect(host = login['host'],
-                       port = login['port'],
-                       user = login['username'],
-                       password = login['password'],
-                       db = 'ImdbDB',
-                       charset = login['charset'])
-
-# MySQL 서버와 연결할 수 있는 cursor 생성
-cursor = conn.cursor()
-
-# read csv
-result_df = pd.read_csv('/Users/gimdohyeon/Documents/스터디/dataset/result_df.csv')
-result_df = result_df[['Imdb_code', 'Title', 'Imdb_Premiere', 'Imdb_Age', 'Imdb_Time', 'IMDB Score', 'Language', 'Nflx_Genre1', 'Nflx_Genre2', 'Nflx_Genre3', 'Nflx_Genre4']] # 컬럼 순서 변경
-result_df = result_df.replace({np.nan: None})
-
-sql = """INSERT INTO ImdbDB.films (imdb_code, title, imdb_premiere, imdb_age, imdb_time, imdb_score, imdb_language, nflx_genre1, nflx_genre2, nflx_genre3, nflx_genre4) 
-            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"""
-
-
-for index, row in result_df.iterrows():
-    cursor.execute(sql, tuple(row))
-
-conn.commit()
-
-cursor.close() # cursor 닫기
-conn.close() # db connection 닫기
-
-
 
 
 # Imdb 사이트 크롤링 : imdb_code를 이용해서 나라별 평가 점수와 포스터 이미지 수집
-
-country_dict = {}  # (key: imdb_code, value: imdb site's json)
-poster_dict = {} # (key: imdb_code, value: poster img)
-error = list()
-
-for index, row in tqdm(result_df.iterrows(), total = result_df.shape[0]):
-
-    code = row['Imdb_code']
-    url = 'https://www.imdb.com/title/' + code + '/ratings/'
-    time.sleep(2)
-
-    session = requests.Session() # cookie 기록을 남겨서 scraper 차단 방지
-    response = requests.get(url, headers = {'User-Agent' :
-                                                'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36'}) # user-agent 설정
-
-    try:
-        if response.status_code == 200:
-            soup = BeautifulSoup(response.text, 'html.parser').find('body')
-
-            # html parsing
-            dom = etree.HTML(str(soup)) # page로부터 html parsing
-            element = dom.xpath('//*[@id="__NEXT_DATA__"]/text()')[0]  # xpath를 통해서 Json 추출
-
-            # json
-            json_object = json.loads(element)
-            j_object = json_object['props']['pageProps']['contentData']  # poster image, rating 정보가 있는 부분
-
-            # extract data from json
-            poster_img = j_object['entityMetadata']['primaryImage']['url']  # films poster image
-            country_data = j_object['histogramData']['countryData']  # country rates and voteCount info
-
-            # make dict (key: imdb_code, value: imdb site's json)
-            country_dict[code] = country_data
-            poster_dict[code] = poster_img
-
-    except:
-        error.append(code) # rating 정보를 제공하지 않는 경우
-        print('error')
+#
+# country_dict = {}  # (key: imdb_code, value: imdb site's json)
+# poster_dict = {} # (key: imdb_code, value: poster img)
+# error = list()
+#
+# for index, row in tqdm(result_df.iterrows(), total = result_df.shape[0]):
+#
+#     code = row['Imdb_code']
+#     url = 'https://www.imdb.com/title/' + code + '/ratings/'
+#     time.sleep(2)
+#
+#     session = requests.Session() # cookie 기록을 남겨서 scraper 차단 방지
+#     response = requests.get(url, headers = {'User-Agent' :
+#                                                 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36'}) # user-agent 설정
+#
+#     try:
+#         if response.status_code == 200:
+#             soup = BeautifulSoup(response.text, 'html.parser').find('body')
+#
+#             # html parsing
+#             dom = etree.HTML(str(soup)) # page로부터 html parsing
+#             element = dom.xpath('//*[@id="__NEXT_DATA__"]/text()')[0]  # xpath를 통해서 Json 추출
+#
+#             # json
+#             json_object = json.loads(element)
+#             j_object = json_object['props']['pageProps']['contentData']  # poster image, rating 정보가 있는 부분
+#
+#             # extract data from json
+#             poster_img = j_object['entityMetadata']['primaryImage']['url']  # films poster image
+#             country_data = j_object['histogramData']['countryData']  # country rates and voteCount info
+#
+#             # make dict (key: imdb_code, value: imdb site's json)
+#             country_dict[code] = country_data
+#             poster_dict[code] = poster_img
+#
+#     except:
+#         error.append(code) # rating 정보를 제공하지 않는 경우
+#         print('error')
 
 
 # db upload
-cursor = conn.cursor()
-cursor.execute("""TRUNCATE TABLE ImdbDB.imdb_score""")
-insert_sql = """INSERT INTO ImdbDB.imdb_score(imdb_code, json_data) VALUES (%s, %s)"""
-update_sql = """UPDATE ImdbDB.imdb_score
-                    SET poster_img = %s
-                    WHERE imdb_code = %s"""
+# cursor = conn.cursor()
+# cursor.execute("""TRUNCATE TABLE ImdbDB.imdb_score""")
+# insert_sql = """INSERT INTO ImdbDB.imdb_score(imdb_code, json_data) VALUES (%s, %s)"""
+# update_sql = """UPDATE ImdbDB.imdb_score
+#                     SET poster_img = %s
+#                     WHERE imdb_code = %s"""
 
 
 # insert mysql with ratings info(json) and poster image
-for key, value in country_dict.items():
-    code = key
-    json_data = json.dumps(value)
-
-    if json_data != '[]':
-        values = (key, json_data)
-        cursor.execute(insert_sql, values)
-
-        img = poster_dict[code]
-        cursor.execute(update_sql, (img, code))
-
-conn.commit()
+# for key, value in country_dict.items():
+#     code = key
+#     json_data = json.dumps(value)
+#
+#     if json_data != '[]':
+#         values = (key, json_data)
+#         cursor.execute(insert_sql, values)
+#
+#         img = poster_dict[code]
+#         cursor.execute(update_sql, (img, code))
+#
+# conn.commit()
 
 
 
 # table join : 영화에 대한 상세정보, json data, 포스터를 모두 확인할 수 있는 테이블 생성
-cursor = conn.cursor()
-cursor.execute("""CREATE TABLE ImdbDB.films_info AS
-                    SELECT A.imdb_code
-                         , A.title
-                         , A.imdb_premiere
-                         , A.imdb_age
-                         , A.imdb_language
-                         , B.json_data
-                         , B.poster_img
-                    FROM ImdbDB.films AS A
-                    LEFT JOIN ImdbDB.imdb_score AS B
-                    ON A.imdb_code = B.imdb_code""")
-
-conn.commit()
-
-cursor.close()
-conn.close()
+# cursor = conn.cursor()
+# cursor.execute("""CREATE TABLE ImdbDB.films_info AS
+#                     SELECT A.imdb_code
+#                          , A.title
+#                          , A.imdb_premiere
+#                          , A.imdb_age
+#                          , A.imdb_language
+#                          , B.json_data
+#                          , B.poster_img
+#                     FROM ImdbDB.films AS A
+#                     LEFT JOIN ImdbDB.imdb_score AS B
+#                     ON A.imdb_code = B.imdb_code""")
+#
+# conn.commit()
+#
+# cursor.close()
+# conn.close()
--- ImdbScores/visualization.py
+++ ImdbScores/visualization.py
@@ -5,7 +5,6 @@ import json
 sys.path.insert(0, './ImdbScores') # mysql 접속정보 저장한 폴더경로 연결
 import mysql_connect # mysql 접속정보
 
-
 app = Flask(__name__)
 login = mysql_connect.info
 
@@ -18,12 +17,14 @@ conn = pymysql.connect(host = login['host'],
                        charset=login['charset'])
 
 
-# 영화 리스트를 10개씩 확인하기 위해 function 정의
+# function: 영화 리스트를 페이지 넘겨서 확인하기
 def fetch_films(page_number):
     cursor = conn.cursor() # 데이터 접근
 
     limit = 10
     offset = (page_number-1) * limit
+
+    # Imdb score 높은 순으로 데이터 가져오기
     sql = f"""SELECT imdb_code, title, imdb_premiere, imdb_age, imdb_time, imdb_language, imdb_score, nflx_genre1, nflx_genre2, nflx_genre3, nflx_genre4
                 FROM ImdbDB.films
                 ORDER BY imdb_score DESC
@@ -34,8 +35,12 @@ def fetch_films(page_number):
 
     return film_data
 
+
+# function: 영화 상세정보 가져오기
 def fetch_films_detail(imdb_code):
     cursor = conn.cursor()
+
+    # 특정 Imdb code에 대한 데이터 가져오기
     sql = f"""SELECT imdb_code, title, imdb_premiere, imdb_age, imdb_language, json_data, poster_img
                 FROM ImdbDB.films_info
                 WHERE imdb_code = '{imdb_code}'"""
@@ -45,18 +50,23 @@ def fetch_films_detail(imdb_code):
 
     return film_data
 
-def visualize_barChart(data):
+
+# function: 영화 평점 시각화해서 이미지 저장
+def visualize_barChart(film_data):
 
     import matplotlib
     matplotlib.use('Agg') # non-interactive backend
     import matplotlib.pyplot as plt
 
-    id = data[0][0]
-    json_data = json.loads(data[0][5])
+    if film_data[0][5] is None: # json data 정보가 없는 경우는 Pass
+        return None
+
+    id = film_data[0][0] # imdb_code
+    json_data = json.loads(film_data[0][5]) # json data
 
     countries = [j['displayText'] for j in json_data]
     rates = [j['aggregateRating'] for j in json_data]
-    avg_rate = sum(rates) / len(rates)  # ratings 평균
+    avg_rate = round(sum(rates) / len(rates), 2) # ratings 평균
 
     plt.figure(figsize=(10, 8))
     plt.bar(countries, rates, label='rating',
@@ -66,25 +76,26 @@ def visualize_barChart(data):
     plt.ylim([0, 10])  # y축범위
     plt.xticks(fontsize=13)
     plt.yticks(fontsize=13)
-    plt.title('Imdb Ratings by Country', pad=10, fontdict={'fontweight': 'bold', 'size': 20})  # 타이틀(pad: 그래프와의 간격)
-    plt.legend(fontsize=13, loc='upper right')  # 범례위치, 폰트크기 설정
+    plt.title('Imdb Ratings by Country', pad=15, fontdict={'fontweight': 'bold', 'size': 20})  # 타이틀(pad: 그래프와의 간격)
 
-    # 그래프에 평점 value 표시하기
+    # 그래프에 나라별 평점 값 표시하기
     for i in range(len(countries)):
         plt.text(countries[i], rates[i], rates[i], ha='center', va='bottom', size=13)  # plt.text(x,y,z) -> (x,y)위치에 z값표시
 
-    plt.axhline(y=avg_rate, color='darkgrey', linestyle='--', label='Average Rating')  # 평균값을 수평선으로 표시
-    plt.legend(fontsize=13)
+    plt.axhline(y=avg_rate, color='darkgrey', linestyle='--', label=f'Average Rating: {avg_rate}')  # 평균값을 수평선으로 표시
 
-    # 참고. 프로젝트 폴더에 static directory가 만들어져 있어야 한다.
+    plt.legend(fontsize=14, loc='upper center', bbox_to_anchor=(0.5, -0.15)) # 범례위치, 폰트크기 설정
+    plt.subplots_adjust(bottom=0.25)
+
+    # 참고. 파이썬 프로젝트 폴더에 static/image directory가 만들어져 있어야 한다.
     # static directory -> Flask가 해당 디렉터리에 있는 정적인 파일을 찾아서 Client에게 제공한다.
-    image = 'static/rate_chart.png'
-    plt.savefig(image)
+    image_name = f'image/rate_chart_{id}.png' # 영화별로 이미지 파일명 다르게 저장
+    plt.savefig(f'static/{image_name}')
 
-    return image
+    return image_name
 
 
-@app.route('/') # 메인화면
+@app.route('/') # 메인화면page
 def film_info():
     # page number
     page_number = int(request.args.get('page', 1))
@@ -92,12 +103,13 @@ def film_info():
     # film info with page
     films = fetch_films(page_number)
 
-    # 참고.html 파일은 하위 templates 폴더 아래에 존재해야 한다.
+    # 참고.html 파일은 templates 폴더 아래에 존재해야 한다.
     return render_template('films_info.html', films = films, page_number = page_number)
 
 
-@app.route('/film/<imdb_code>') # 영화 상세화면
+@app.route('/film/<imdb_code>') # 영화상세정보page
 def show_film(imdb_code):
+
     film = fetch_films_detail(imdb_code)
     chart_image = visualize_barChart(film)
 
